c32d0ce901f8ed6427d353efd2331e17e2937bd9
briancurt <bcurtich@gmail.com>
2018-12-19 16:11:34 -0300

    Refactor

---
+++ scheduled.py
@@ -0,0 +1,83 @@
+import logging
+import os
+import random
+import requests
+import time
+import yaml
+from slackclient import SlackClient
+
+
+slack_token = os.environ["SLACK_API_TOKEN"]
+sc = SlackClient(slack_token)
+config = yaml.load(open("config.yaml"))
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
+
+
+def reminder():
+
+    rcol = lambda: random.randint(0, 255)
+    bb_user = os.environ["BITBUCKET_USER"]
+    bb_pass = os.environ["BITBUCKET_PASS"]
+    server_url = config["general"]["server_url"]
+    default_title = "_Still waiting to be reviewed and addressed:_"
+
+
+    for project_name, this_project in config['projects'].items():
+
+        attr = []
+        slack_channel = this_project['channel']
+        project_url = f"{server_url}/rest/api/1.0/projects?name={project_name}"
+        r = requests.get(project_url, auth=(bb_user, bb_pass))
+        project_key = r.json()['values'][0]['key']
+
+        for repo in this_project['repos']:
+
+            repo_link = f"<{server_url}/projects/{project_key}/repos/{repo}/browse|{repo}/>"
+            prs_endpoint = f"{server_url}/rest/api/1.0/projects/{project_key}/repos/{repo}/pull-requests?state=OPEN"
+            subtext = ""
+
+            r = requests.get(prs_endpoint, auth=(bb_user, bb_pass))
+            result_json = r.json()
+
+            if result_json["values"]:
+                for pr in result_json["values"]:
+
+                    seconds_ago = int(time.time()) - int(str(pr["createdDate"])[:10])
+                    days_ago = int(seconds_ago / 86400)
+                    hours_ago = int((seconds_ago / (60 * 60)) % 24)
+
+                    if days_ago > 1:
+                        time_ago = f"{str(days_ago)} days ago"
+                    elif days_ago == 1:
+                        time_ago = f"{str(days_ago)} day ago"
+                    elif hours_ago > 1:
+                        time_ago = f"{str(hours_ago)} hours ago"
+                    elif hours_ago <= 1:
+                        time_ago = "Just created"
+
+                    subtext += (
+                        "<"
+                        + pr["links"]["self"][0]["href"]
+                        + "|PR-"
+                        + str(pr["id"])
+                        + "> | _"
+                        + time_ago
+                        + "_ | "
+                        + pr["title"]
+                        + "\n"
+                    )
+
+                attr.append(
+                    {
+                        "color": "#%02X%02X%02X" % (rcol(), rcol(), rcol()),
+                        "title": repo_link,
+                        "text": subtext,
+                        "mrkdwn_in": ["text"],
+                    }
+                )
+
+        if not attr:
+            default_title = "There are no pending PRs! :star:"
+
+        sc.api_call("chat.postMessage", channel=slack_channel, text=default_title, attachments=attr)
